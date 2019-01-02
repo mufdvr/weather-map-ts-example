00cdb1cc94f509a2801801fe188af4340a88409f
mufdvr <belora2010@yandex.ru>
2019-01-03 01:46:38 +0300

    commit

--- src/utils.tsx
+++
@@ -1,4 +0,0 @@
-export const checkResponse = (response: any) =>
-  new Promise((resolve, reject) => {
-    response.ok ? response.json().then((json: any) => resolve(json)) : response.json().then((json: any) => reject(json))
-  })
